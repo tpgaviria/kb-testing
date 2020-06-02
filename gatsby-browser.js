159c67ba6bb19d4c83f995fd420e632f43ef6414
Tanya Gaviria <tpgaviria@gmail.com>
2020-06-02 14:46:53 -0400

    styling changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 import "bootstrap";
 import "jquery/dist/jquery.min.js";
 import "popper.js";
 import "./src/styles/global.css"
-=======
-import "./src/styles/global.css"
-import "bootstrap";
-import "jquery/dist/jquery.min.js";
-import "popper.js";
->>>>>>> 8c3c2103ea7bec05787c9c1b22ca5aedec07ddc0
