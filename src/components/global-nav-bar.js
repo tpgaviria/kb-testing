09f7301fdb1b358af9d9ad7a4ab139ab51324893
Tanya Gaviria <tpgaviria@gmail.com>
2020-06-02 14:43:53 -0400

    styling changes

--- src/components/global-nav-bar.js
+++ src/components/global-nav-bar.js
@@ -1,34 +1,39 @@
 // import { Link } from "gatsby"
 // import PropTypes from "prop-types"
 import React from "react"
+import Logo from "../images/PDLogoNav.png"
 
 const GlobalNav = ({ siteTitle }) => (
-  <nav class="navbar navbar-expand-lg navbar-dark" style={{background: `#A4C5E0`}}>
-    <a class="navbar-brand align-middle" href="/">PagerDuty | Knowledge Base</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+  <nav className="navbar navbar-expand-lg navbar-dark pb-0">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="navbar-brand" id="homeLink" href="/docs"><img src={Logo} className="d-inline-block align-middle mx-auto" height="25" alt="PagerDuty"/> | Knowledge Base</a>
+      </li>
+    </ul>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-          <a class="nav-link" href="/">Knowledge Base</a>
+    <div className="collapse navbar-collapse" id="navbarText">
+      <ul className="navbar-nav ml-5">
+        <li className="nav-item active">
+          <a className="nav-link" href="/docs">Knowledge Base</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.pagerduty.com/integrations/">Integrations</a>
+        <li className="nav-item">
+          <a className="nav-link" href="https://www.pagerduty.com/integrations/">Integrations</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://developer.pagerduty.com/">API Docs</a>
+        <li className="nav-item">
+          <a className="nav-link" href="https://developer.pagerduty.com/">API Docs</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://community.pagerduty.com/forum/">Community Discussions</a>
+        <li className="nav-item">
+          <a className="nav-link" href="https://community.pagerduty.com/forum/">Community Discussions</a>
         </li>
       </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="https://tickets.pagerduty.com/hc/en-us/requests/new">Contact Support</a>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="https://tickets.pagerduty.com/hc/en-us/requests/new">Contact Support</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://tickets.pagerduty.com/hc/en-us/requests">My Support Tickets</a>
+        <li className="nav-item">
+          <a className="nav-link" href="https://tickets.pagerduty.com/hc/en-us/requests">My Support Tickets</a>
         </li>
       </ul>
     </div>
