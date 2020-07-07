6f29840fc1bf1f49dbc3e516f96218b5b1ade713
Tanya Gaviria <tpgaviria@gmail.com>
2020-07-07 13:28:37 -0400

    added GlobalNav component to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,6 +24,7 @@ const Layout = ({ children }) => {
       <Helmet>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
       </Helmet>
+      <GlobalNav />
       <div
         style={{
           margin: `0 auto`,
