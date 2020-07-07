cffd5f11c9145959ef7186b015ed5da0c23ce4ec
Tanya Gaviria <tpgaviria@gmail.com>
2020-07-07 18:35:36 -0400

    article grid template

--- src/components/landing-page-articles.js
+++ src/components/landing-page-articles.js
@@ -2,19 +2,66 @@ import React from "react"
 import Logo from "../images/PDLogoNav.png"
 
 const LandingPageArticles = () => (
-<div class="container">
-  <div class="row">
-    <div class="col-sm">
-      One of three columns
+    <div class="container">
+        <div class="row">
+            <h2>Most Viewed Topics</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <h6>Getting Started</h6>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <h6>Incidents and Alerts</h6>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <h6>Services and Integrations</h6>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <h2>Browse All Topics</h2>
+        </div>
+        <div class="row">
+            <div class="col-sm">
+                <h6>Getting Started</h6>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <h6>Incidents and Alerts</h6>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                </ul>
+            </div>
+            <div class="col-sm">
+                <h6>Services and Integrations</h6>
+                <ul>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                    <li>Link 1</li>
+                </ul>
+            </div>
+        </div>
     </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-    <div class="col-sm">
-      One of three columns
-    </div>
-  </div>
-</div>
 )
 
 
