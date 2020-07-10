99f370b9ec6d63a75f9dd698fae061361be85102
Jade Paoletta <jadepaoletta27@gmail.com>
2020-07-10 11:50:39 -0700

    changes to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,10 +8,10 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import { Helmet } from "react-helmet"
 
-import Header from "./header"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./layout.css"
+import Header from "./header"
 import GlobalFooter from "./global-footer"
 
 const Layout = ({ children }) => {
@@ -28,9 +28,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <Helmet>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
-      </Helmet>
       <div
         style={{
           margin: `0 auto`,
